7637d3f76a83a080cda9a41cca22da57873d25a1
yuy <yuyang_keep@163.com>
2019-12-18 10:11:34 +0800

    12/17/23

---
+++ app/src/main/java/com/yuy/demos/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.yuy.demos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Coder: yuyang
+ * Date:  2019/12/4
+ * Description:
+ * Version:
+ */
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
